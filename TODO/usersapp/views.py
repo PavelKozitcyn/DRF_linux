7cd59d3ae4d0cb7f8bde37913f25f8769849b0c7
paul <kpasha11@mail.ru>
2023-03-07 18:11:47 +0300

    apd_userapp_views

--- TODO/usersapp/views.py
+++ TODO/usersapp/views.py
@@ -1,8 +1,13 @@
+from rest_framework import mixins, viewsets
 from rest_framework.viewsets import ModelViewSet
 from .models import CustomUser
 from .serializers import UserSerializer
 
+
 # Create your views here.
-class UserViewSet(ModelViewSet):
+class UserViewSet(mixins.ListModelMixin,
+                  mixins.RetrieveModelMixin,
+                  mixins.UpdateModelMixin,
+                  viewsets.GenericViewSet):
     serializer_class = UserSerializer
     queryset = CustomUser.objects.all()
